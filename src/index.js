09d358291bb1d0cb2e727c91b5eb5e040f382dcd
yanyanzl <jiangzhanling@gmail.com>
2026-01-15 21:00:26 +0000

    app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AstrologyApp from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AstrologyApp />);
 
 
 if ('serviceWorker' in navigator) {
